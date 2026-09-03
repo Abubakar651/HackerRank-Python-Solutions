b8bb80e1b21b7ad81e7e56b46bf70a36b4ffd26e
Abubakar651 <126201169+Abubakar651@users.noreply.github.com>
2026-09-03 22:49:29 +0500

    ✅ Write a function | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-03

--- hackerrank/medium/write-a-function/solution.py
+++ hackerrank/medium/write-a-function/solution.py
@@ -1,4 +1,14 @@
-if __name__ == '__main__':
-    n = int(input())
-    for i in range(0,n):
-        print (i*i)
+def is_leap(year):
+    leap = False
+    if (year%400==0):
+        return True
+    elif(year%100==0):
+        return False
+    elif(year%4==0):
+        return True
+    else:
+        return False
+    
+    
+    return leap
+
